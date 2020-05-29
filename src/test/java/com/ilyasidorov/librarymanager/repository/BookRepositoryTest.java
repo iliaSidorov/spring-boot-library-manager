f44232873ec612484219138803e311c60b4341fc
Ilya Sidorov <ilyasidorov114@gmail.com>
2020-05-29 16:36:11 +0300

    Set test environment

--- src/test/java/com/ilyasidorov/librarymanager/repository/BookRepositoryTest.java
+++ src/test/java/com/ilyasidorov/librarymanager/repository/BookRepositoryTest.java
@@ -1,33 +1,27 @@
 package com.ilyasidorov.librarymanager.repository;
-
 import com.ilyasidorov.librarymanager.domain.Book;
 import com.ilyasidorov.librarymanager.service.BookService;
-import org.junit.ClassRule;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.testcontainers.containers.MySQLContainer;
 import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import java.util.List;
 
-
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class BookRepositoryTest {
-
-    @ClassRule
-    public static MySQLContainer mySQLContainer =
-            new MySQLContainer("mysql:8")
-                    .withDatabaseName("librarymanager")
-                    .withUsername("librarian")
-                    .withPassword("librarian");
 
     @Autowired
     private BookService bookService;
 
+    @Test
+    public void findAllTest() {
+        List<Book> books = bookService.findBooksByOrderById();
+        assertThat(books.size()).isEqualTo(10);
+    }
 
-
-
-
+    
 }
